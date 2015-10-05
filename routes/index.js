b598c0d274ad9fc24588b290ccb1eafc98e0a07c
cipher1729 <cipher1729@gmail.com>
2015-10-04 19:31:35 -0700

    with table display of fields

--- routes/index.js
+++ routes/index.js
@@ -15,12 +15,43 @@ router.get('/records', function(req, res, next) {
 	 //res.render('records', { title: 'All records', records: JSON.stringify(docs), something:'hello' });
 	res.render('records', { title: 'All records', records: docs, something:'hello' });
   });
-  
- 
 });
 
 
 /*POST a new record*/
+router.post('/addRecord',function(req, res, next) {
+  var db = req.db;
+  var collection= db.get('usercollection');
+  var uuid= req.body.uuid;
+  var camera_id= req.body.camera_id;
+  var site_id = req.body.site_id;
+  var image_width= req.body.image_width;
+  var image_height = req.body.image_height;
+  var epoch_time = req.body.epoch_time;
+  var processing_time_ms= req.body.processing_time_ms;
+  var results = req.body.results;
+   collection.insert({
+        "uuid" : uuid,
+		"camera_id":camera_id,
+        "site_id" : site_id,
+		"image_width" : image_width,
+		"image_height" : image_height,
+		"epoch_time" : epoch_time,
+		"processing_time_ms" : processing_time_ms,
+		"results" : results
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect("records");
+        }
+    });
+});
+
+
 
 
 
