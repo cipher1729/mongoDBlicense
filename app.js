20bde1c1686e24747173ec9fc216d9634b6ab21f
cipher1729 <cipher1729@gmail.com>
2015-10-17 23:55:55 -0700

    basic working. Also shows user details

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req,res,next){
 	req.db= db;
+	 res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	//console.log(db);
 	next();
 });
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,271 @@ var router = express.Router();
 var assert = require('assert');
 var fs= require('fs');
 var image={};
+var firstCar = true;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+//for user to validate credentials
+router.get('/login', function(req, res, next){
+	
+	var db = req.db;
+	var collection= db.get('usercollection');
+	var phone= req.query.phone;
+	var password=req.query.password;
+	
+	console.log(phone);
+	var licensePlate={};
+	collection.find({"phone": phone},{}, function(e,docs){
+			 console.log("e is"+ e);
+			 //does not exist in database
+			 if(docs.length==0)
+			 res.send("Not found!");
+			 else
+			 {
+				//account is found need to find password
+				if(docs[0].password== password)
+					res.send("Validated!");
+				else
+					res.send("Password did not match!");
+			 }
+	});
+  
+  
+	
+  
+});
+
+//for user to register in an account
+router.post('/signup', function(req, res, next){
+ 
+  var db = req.db;
+  var collection= db.get('usercollection');
+  
+  //get user's name, phone, email and licensePlateNumber
+ var name=req.body.name; 
+ var phone= req.body.phone;
+ var password= req.body.password;
+  var email=req.body.email;
+  var licensePlate=req.body.licensePlate;
+  var timeIn=0;
+  var timeOut=0;
+  
+  //find if phone already exists
+  collection.find({"phone":phone},{}, function(e,docs){
+   if(docs.length!=0)
+		res.send("Already Exists!");
+   else
+	//if not insert this entry into the database
+		collection.insert({
+		"name":name,
+        "phone" : phone,
+		"password":password,
+		"email":email,
+		"licensePlate": licensePlate,
+		"timeIn":timeIn,
+		"timeOut":timeOut
+		}, function (err, doc) {
+			if (err) {
+				// If it failed, return error
+				res.send("There was a problem adding the information to the database.");
+			}
+			else {
+				res.send("Added to database");
+			}
+		});
+   });
+});
+
+function populateLots(collection)
+{
+	//populate the lotcollection
+	for(var i=0;i<21;i++)
+	{
+		collection.insert({"cameraId":i, "occupied": false},{});
+	}
+	
+}
+
+/* for indicating that car has entered database, to be called by entry camera*/
+router.post('/createdetail', function(req,res,next){
+	var cameraId = parseInt(req.body.cameraId);
+	var licensePlate= req.body.licensePlate;
+	var db = req.db;
+    
+	
+	var collection= db.get('detailcollection');
+  
+	//populate lotcollection if this is the first car
+	if(firstCar==true)
+	populateLots(db.get('lotcollection'));
+	
+	//find if phone already exists
+    collection.find({"cameraId":cameraId},{}, function(e,docs){
+	//if not insert this entry into the database
+		collection.insert({
+		"cameraId":cameraId,
+        "licensePlate" : licensePlate
+		}, function (err, doc) {
+			if (err) {
+				// If it failed, return error
+				res.send("There was a problem adding the information to the detail database.");
+			}
+			else {
+				res.send("Detail Added to database");
+			}
+		});
+   });
+   
+   //log entry time for user
+	
+		var userCollection = db.get('usercollection');
+		userCollection.update({"licensePlate":licensePlate},{$set:{"timeIn":new Date().getTime() / 1000}}); 
+});
+
+
+
+/* for updating detail record by some camera */
+router.post('/updatedetail', function(req,res,next){
+	var cameraId = parseInt(req.body.cameraId);
+	var licensePlate = req.body.licensePlate;
+	//console.log(cameraId);
+	var db = req.db;
+    var detailCollection= db.get('detailcollection');
+	var lotCollection= db.get('lotcollection');
+    var userCollection= db.get('usercollection');
+   
+   console.log(licensePlate);
+	//find if phone already exists
+   
+   detailCollection.find({"licensePlate":licensePlate},{}, function(e,docs){
+   
+	if(docs.length!=0)
+	{
+					//SET IT TO OCCUPIED
+					lotCollection.find({"cameraId":cameraId},{},function(e,docs){
+								console.log(docs);
+								if(docs.length==0)
+								res.send("No lot entry for this id exists");
+								else
+								{
+								//if id is either 0 or 20 the lot has just become free. Else it has just become occupied
+									if(cameraId==20)
+									{	//user is exiting parking lot
+										console.log("Exiting!");
+										var actualCameraId={};
+										//get cameraId first and then remove
+										detailCollection.find({"licensePlate":licensePlate},{},function(e, docs){
+											
+											actualCameraId = docs[0].cameraId;
+											console.log(docs);
+											
+											detailCollection.remove({"licensePlate":licensePlate}); 
+											console.log("removed licenseplate " + licensePlate );
+										
+											//log exit time for user
+												userCollection.find({"licensePlate": licensePlate},{},function(e,docs){
+													userCollection.update({"licensePlate":licensePlate},{$set:{"timeOut":new Date().getTime() / 1000}}); 
+											});
+
+											
+											lotCollection.update({"cameraId":actualCameraId},{$set:{"occupied":false}}); 
+											//record his exit time
+										
+										
+										
+										});
+									
+										
+										
+										
+									}
+									else
+									{
+										console.log(" Non 20 block ");
+										lotCollection.update({"cameraId":cameraId},{$set:{"occupied":true}}); 
+										detailCollection.update({"licensePlate":licensePlate},{$set:{"cameraId":cameraId}}); 
+									}
+								}
+					});
+    }
+	else
+		res.send("Cannot update. CameraId does not exist in database.");
+	});
+});
+
+
+router.get('/findlot', function(req, res, next){
+	var db = req.db;
+    
+	//get the phone number, find the corresponding license and then use the license to retrieve details
+	var phone= req.query.phone;
+	
+	console.log(phone);
+	var userCollection = db.get('usercollection');
+	
+			userCollection.find({"phone":phone},{},function(e,docs){
+				var licensePlate = docs[0].licensePlate;
+			
+				console.log(" to find " + licensePlate); 
+					
+					var collection= db.get('lotcollection');
+					collection.find({},{}, function(e,docs){
+					
+						//send all records
+						
+						var allRecords = docs;
+						var detailCollection= db.get('detailcollection');
+						detailCollection.find({"licensePlate":licensePlate},{},function(e,docs){
+					
+							if(docs.length==0)
+							{
+									var sendObj= {"results": allRecords, "cameraId":0,"licensePlate": licensePlate};
+									var sendString = JSON.stringify(sendObj); 
+									res.send(sendString);
+							}
+							else
+							{
+								var sendObj= {"results": allRecords, "cameraId":(docs[0].cameraId),"licensePlate": licensePlate};
+								var sendString = JSON.stringify(sendObj); 
+								res.send(sendString);
+							}
+					
+					});
+						
+			});
+	});
+	
+	//get the users licensePlate
+	//var licensePlate = req.query.licensePlate;
+});
+
+//for getting user details, wants the phone as query parameter
+router.get('/userdetails', function(req,res,next){
+	var db = req.db;
+    
+	//get the phone number, find the corresponding license and then use the license to retrieve details
+	var phone= req.query.phone;
+	console.log(phone);	
+	var userCollection = db.get('usercollection');
+	userCollection.find({"phone":phone},{},function(e,docs){
+		if(docs.length==0)
+		res.send("Can't find user");
+		else
+		{	//time diff in seconds
+			var diff = (docs[0].timeOut - docs[0].timeIn)/1000;
+			var payment = diff * 0.5;
+			var resultObject = {"payment":payment, "timeSpent": diff, "name": docs[0].name, "phone": docs[0].phone, "email":docs[0].email, "licensePlate":docs[0].licensePlate};
+			res.send(JSON.stringify(resultObject));
+		}
+	});
+
+
+});
+/*
 router.get('/records', function(req, res, next) {
   var db = req.db;
   var collection= db.get('usercollection');
@@ -22,9 +281,6 @@ router.get('/records', function(req, res, next) {
 	res.render('records', { title: 'All records', records: docs, something:'hello' });
   });
 });
-
-
-/*POST a new record*/
 router.post('/addRecord',function(req, res, next) {
   var db = req.db;
   var collection= db.get('usercollection');
@@ -60,8 +316,60 @@ router.post('/addRecord',function(req, res, next) {
     });
 });
 
+router.post('/addUser',function(req, res, next) {
+  var db = req.db;
+  var collection= db.get('logincollection');
+  var username= req.body.username;
+  var password= req.body.password;
+ 
+   collection.find({"username":username,"password":password},{}, function(e,docs){
+   if(docs.length!=0)
+		res.send("Exists!");
+   else
+		collection.insert({
+        "username" :username,
+		"password":password
+		}, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+			
+        }
+        else {
+            // And forward to success page
+            console.log(username);
+			console.log(password);
+			res.send("Success!");
+        }
+		});
+   });
+   
+});
 
+router.get('/checkUser', function(req, res, next) {
+  var db = req.db;
+  var collection= db.get('logincollection');
+  var username= req.param('username');
+  var password= req.param('password');
+ 
+   console.log(username);
+   console.log(password);
+  collection.find({"username": username},{}, function(e,docs){
+	 console.log("e is"+ e);
+	 var found=false;
+	 for(var k=0;k<docs.length;k++)
+	 {
+		 if(docs[k].password == password) 
+		 found=true;
+	}
+	if(found==true)
+	res.send("Found!");
+	else
+	res.send("Not found :(");
+  });
+});
 
+*/
 
 
 module.exports = router;
